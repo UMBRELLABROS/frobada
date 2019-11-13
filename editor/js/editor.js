e7548c884498e45643b29db227e743ab1f97222e
Thomas Bruckmann <tbruckmann@mmm.com>
2019-11-13 13:02:13 +0100

    colorHex

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -24,11 +24,19 @@ function saveDOM(event) {
   var source = event.target.getAttribute('data-source');
   elements = [];
   elements = scanDOM(source, elements);
+  // string has only numbers
+  // ^ start [0-9] numbers + more of that kind $ end
+  var reg = new RegExp('^[0-9]+$');
   // for each DIV scan the style properties !=""
+  // get the id
+  // get the data-attributes
+  // get the functions
+  // get the innerHTML
   elements.forEach(element => {
-    styles = element.style;
+    styles = element.style;  // read the inline styles
     Object.keys(styles).forEach(style => {
-      if (styles[style]) {
+      // new styles without the number-list
+      if (styles[style] && !reg.test(style)) {
         console.log(style); // key
         console.log(styles[style]); // value
       }
@@ -45,4 +53,16 @@ function scanDOM(source, elements) {
     }
   });
   return elements;
+}
+
+function colorToHex(color) {
+  // Convert any CSS color to a hex representation
+  // Examples:
+  // colorToHex('red')            # '#ff0000'
+  // colorToHex('rgb(255, 0, 0)') # '#ff0000'
+  var rgba = colorToRGBA(color);
+  var hex = [0, 1, 2].map(
+    function (idx) { return byteToHex(rgba[idx]); }
+  ).join('');
+  return "#" + hex;
 }
